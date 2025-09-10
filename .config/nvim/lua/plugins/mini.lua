6e6d915c7ade9fb65947a19e2e77ae026ae19879
IlviCumani <ilviCumani@gmail.com>
2025-09-10 09:18:18 +0200

    fix iterm color

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -1,6 +1,5 @@
 -- Define React icon once to avoid duplication across JS/TS files
 -- Change this icon to update all React-related files at once
-local storybook_icon = ""
 
 return {
   {
@@ -12,22 +11,28 @@ return {
   },
   {
     "echasnovski/mini.icons",
-    opts = {
-      filetype = {
-        ["scss"] = { glyph = "", hl = "MiniIconsRed", name = "Scss" },
-      },
-      extension = {
-        ["stories.tsx"] = { glyph = storybook_icon, hl = "MiniIconsRed", name = "StoryBook" },
-        ["stories.jsx"] = { glyph = storybook_icon, hl = "MiniIconsRed", name = "StoryBook" },
-        ["stories.ts"] = { glyph = storybook_icon, hl = "MiniIconsRed", name = "StoryBook" },
-        ["stories.js"] = { glyph = storybook_icon, hl = "MiniIconsRed", name = "StoryBook" },
-        ["tmux"] = { glyph = "", hl = "MiniIconsGreen", name = "Tmux" },
-      },
-      file = {
-        ["next.config.ts"] = { glyph = " ", hl = "MiniIconsGrey", name = "Next Config" },
-        ["postcss.config.mjs"] = { glyph = " ", hl = "MiniIconsOrange", name = "Next Config" },
-        [".tmux.conf"] = { glyph = "", hl = "MiniIconsGreen", name = "Tmux" },
-      },
-    },
+    opts = function()
+      vim.api.nvim_set_hl(0, "TmuxGreenIcon", { fg = "#78bd44" })
+
+      local storybook_icon = ""
+
+      return {
+        filetype = {
+          ["scss"] = { glyph = "", hl = "MiniIconsRed", name = "Scss" },
+        },
+        extension = {
+          ["stories.tsx"] = { glyph = storybook_icon, hl = "MiniIconsRed", name = "StoryBook" },
+          ["stories.jsx"] = { glyph = storybook_icon, hl = "MiniIconsRed", name = "StoryBook" },
+          ["stories.ts"] = { glyph = storybook_icon, hl = "MiniIconsRed", name = "StoryBook" },
+          ["stories.js"] = { glyph = storybook_icon, hl = "MiniIconsRed", name = "StoryBook" },
+          ["tmux"] = { glyph = "", hl = "TmuxGreenIcon", name = "Tmux" },
+        },
+        file = {
+          ["next.config.ts"] = { glyph = " ", hl = "MiniIconsGrey", name = "Next Config" },
+          ["postcss.config.mjs"] = { glyph = "", hl = "MiniIconsOrange", name = "Next Config" },
+          [".tmux.conf"] = { glyph = "", hl = "TmuxGreenIcon", name = "Tmux" },
+        },
+      }
+    end,
   },
 }
